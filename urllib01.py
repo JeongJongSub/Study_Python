d0e9c58db25847c7cfd1c5686c7aad99530b3321
JeongJongSub <jeongjs501@gmail.com>
2025-02-25 21:24:57 +0900

    주석 색 처리

---
+++ urllib01.py
@@ -0,0 +1,11 @@
+# //! request 모듈 사용하기1
+import urllib.request as request
+
+# //* 첫줄이라서 안되는 건가?
+# //! ㅁㄴㅇㄻㄴㅇㄹ  
+# //! ㅁㄴㅇㄻㄴㅇㄹ
+# 완전히 붉은것 보다는 이게 낳을지도?
+# //asdf sdfasdfsaf
+# //*asdfkjlkjf 약간 이건 라임 색이 낮겠넹
+# //* 색 구별이 별로 안된다.
+
